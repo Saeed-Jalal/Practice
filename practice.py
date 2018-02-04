b864676cd413b9a834bc0000eccab34cab69a4b9
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-02-04 17:08:31 +0200

    Practice

---
+++ practice.py
@@ -0,0 +1,22 @@
+
+
+print("What is your name? ")
+myName = input()
+print("It is my pleasure to meet you " + myName )
+
+print("The length of your name is: ")
+print(len(myName))
+
+print("What is your age? ")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
+print("Enter your name:")
+name = input()
+
+if name:
+    print("Good day")
+else:
+    print("You are not welcome")
+
+
+
